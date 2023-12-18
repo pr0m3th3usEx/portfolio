40c1ed477f8004be89b53aeebf2e99b4695939c3
pr0m3th3usEx <thomas1.michel@epitech.eu>
2023-12-19 00:30:39 +0100

    Add image Slider + skill card

---
+++ src/components/widgets/SkillCard.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+const props = defineProps<{ 
+    icon: any;
+    label: string;
+}>();
+</script>
+
+<template>
+    <div class="skill-block">
+        <div class="icon">
+            <props.icon />
+        </div>
+        <p class="label">{{ props.label }}</p>
+    </div>
+</template>
+
+<style scoped>
+
+.skill-block {
+    display: flex;
+    flex-direction: column;
+    gap: 18px;
+    align-items: center;
+    padding: 32px;
+    background: #383735;
+    
+    .icon {
+        width: 48px;
+        color: white;
+    }
+
+    &:hover {
+      background: #4e4a41; 
+    }
+
+    .label {
+        text-align: center;
+    }
+}
+
+</style>
---
+++ src/store/constants/skills.ts
@@ -0,0 +1,73 @@
+import CIcon from "@/components/icons/languages/CIcon.vue";
+import DockerIconVue from "@/components/icons/languages/DockerIcon.vue";
+import FigmaIconVue from "@/components/icons/languages/FigmaIcon.vue";
+import GraphQLVue from "@/components/icons/languages/GraphQL.vue";
+import HerokuIconVue from "@/components/icons/languages/HerokuIcon.vue";
+import MangoDBIconVue from "@/components/icons/languages/MangoDBIcon.vue";
+import NestIconVue from "@/components/icons/languages/NestIcon.vue";
+import NextIconVue from "@/components/icons/languages/NextIcon.vue";
+import NodeIconVue from "@/components/icons/languages/NodeIcon.vue";
+import PostgresIconVue from "@/components/icons/languages/PostgresIcon.vue";
+import PythonIconVue from "@/components/icons/languages/PythonIcon.vue";
+import ReactIconVue from "@/components/icons/languages/ReactIcon.vue";
+import TypescriptIconVue from "@/components/icons/languages/TypescriptIcon.vue";
+import VueIconVue from "@/components/icons/languages/VueIcon.vue";
+
+export default [
+    {
+        icon: NodeIconVue,
+        label: 'Node.js',
+    },
+    {
+        icon: VueIconVue,
+        label: 'Vue',
+    },
+    {
+        icon: ReactIconVue,
+        label: 'React',
+    },
+    {
+        icon: NextIconVue,
+        label: 'Next.js',
+    },
+    {
+        icon: TypescriptIconVue,
+        label: 'Typescript',
+    },
+    {
+        icon: NestIconVue,
+        label: 'NestJS',
+    },
+    {
+        icon: DockerIconVue,
+        label: 'Docker',
+    },
+    {
+        icon: GraphQLVue,
+        label: 'GraphQL',
+    },
+    {
+        icon: PostgresIconVue,
+        label: 'PostgreSQL',
+    },
+    {
+        icon: MangoDBIconVue,
+        label: 'MangoDB',
+    },
+    {
+        icon: PythonIconVue,
+        label: 'Python',
+    },
+    {
+        icon: CIcon,
+        label: 'C',
+    },
+    {
+        icon: HerokuIconVue,
+        label: 'Heroku',
+    },
+    {
+        icon: FigmaIconVue,
+        label: 'Figma',
+    },
+];
