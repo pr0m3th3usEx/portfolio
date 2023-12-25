c0d0883762d371805c3f2ccb22bd7d6970508ce9
pr0m3th3usEx <thomas1.michel@epitech.eu>
2023-12-25 19:07:19 +0100

    Fix favicon + titles

--- src/App.vue
+++ src/App.vue
@@ -20,13 +20,6 @@ const scrollToIntro = () => {
 </script>
 
 <template>
-  <head>
-    <title>Thomas Michel - Software Engineer</title>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="description" content="Software Developer crafting impactful digital experiences with expertise in front-end and back-end technologies." />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-  </head>
   <main>
     <Background />
     <div class="main-content scroll-container">
