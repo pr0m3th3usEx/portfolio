93a2d1e4b554bcd204a6c9df8896f8244f30eae5
pr0m3th3usEx <thomas1.michel@epitech.eu>
2024-09-26 14:55:41 +0200

    add env configurationg

---
+++ src/utils/config.ts
@@ -0,0 +1,10 @@
+import env from 'env-var';
+
+const envConstants = {
+  GITHUB_URL: env.get('NEXT_PUBLIC_GITHUB_URL').required().asUrlString(),
+  LINKEDIN_URL: env.get('NEXT_PUBLIC_LINKED_URL').required().asUrlString(),
+  RESUME_DOWNLOAD_URL: env.get('NEXT_PUBLIC_RESUME_URL').required().asUrlString(),
+  EMAIL_PRO: env.get('NEXT_PUBLIC_EMAIL').required().asString(),
+};
+
+export default envConstants;
