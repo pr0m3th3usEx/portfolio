9706c8e017618559de39a390139186f0b999af47
pr0m3th3usEx <thomas1.michel@epitech.eu>
2025-03-08 09:47:04 +0100

    base repo

--- src/app/components/widgets/accordion/content.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const AccordionContent = ({ children }: { children: React.ReactNode }) => {
-  return <React.Fragment key="acc-content">{children}</React.Fragment>;
-};
-
-export default AccordionContent;
