aa8b7dcefb074263924b117f0721898238a959ea
pr0m3th3usEx <thomas1.michel@epitech.eu>
2025-03-21 09:04:27 +0100

    fix header zIndex

--- components/header.tsx
+++ components/header.tsx
@@ -9,7 +9,7 @@ import Clock from './clock';
 
 export default function Header() {
   return (
-    <header className="flex w-full justify-between px-4 pt-8 md:fixed md:top-0 md:px-4">
+    <header className="z-10 flex w-full justify-between px-4 pt-8 md:fixed md:top-0 md:px-4">
       <NavigationBar />
 
       <div className="flex items-center gap-2">
